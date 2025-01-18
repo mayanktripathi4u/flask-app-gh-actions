7166484fe1379817c546e74f5b525959433253df
mayanktripathi4u <mayanktripathi4u@gmail.com>
2025-01-17 23:53:55 -0600

    added test file

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app # Import the Flask app from your main application file (app.py)
+
+class TestFlaskApp(unittest.TestCase):
+    def setUp(self):
+        # Set up test client for the Flask App
+        self.app = app.test_client()
+        self.app.testing = True # Enable Testing Mode
+
+    def test_welcome(self):
+        # Send get request to the root url.
+        response = self.app.get('/')
+        # Check if the status code is 200
+        self.assertEqual(response.status_code, 200)
+        # Check if the response JSON contains the expected message
+        self.assertEqual(response.get_json(), {"message": "You are welcome!"})
+
+if __name__ == '__main__':
+    unittest.main()
+
